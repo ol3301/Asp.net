67f6590a0291f5fdded255ffcbb40ebe149d19ed
mazda <ol.kyzy@gmail.com>
2019-04-07 11:45:32 +0300

    party project

---
+++ Party/Party/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Party.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Greeting = DateTime.Now.Hour < 12 ? "Доброго утра" : "Доброго дня";
+            return View();
+        }
+    }
+}
