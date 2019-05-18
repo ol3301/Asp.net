4778218ff0736783335631cf87c65a8c4e5f6990
Олег Кизименко <ol.kyzy@gmail.com>
2019-05-18 16:12:21 +0300

    итоговая работа

---
+++ protoshop/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using shoppingstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shoppingstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        ShoppingStoreEntities StoreDB = new ShoppingStoreEntities();
+        private List<Item> GetTopSellingItems(int count)
+        {
+            return StoreDB.Items.OrderByDescending(i => i.OrderDetails.Count())
+                .Take(count)
+                .ToList();
+        }
+        public ActionResult Index()
+        {
+            var items = GetTopSellingItems(10);
+            return View(items);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ protoshop/Controllers/StoreController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using shoppingstore.Models;
+namespace shoppingstore.Controllers
+{
+    public class StoreController : Controller
+    {
+        ShoppingStoreEntities storeDB = new ShoppingStoreEntities();
+        // GET: Store
+        public ActionResult Index()
+        {
+            var categories = storeDB.Categories.ToList();
+            
+            return View(categories);
+        }
+        [ChildActionOnly]
+        public ActionResult CategoryMenu()
+        {
+            var categories = storeDB.Categories.ToList();
+            return PartialView(categories);
+
+        }
+        public ActionResult Browse(string category)
+        {
+            var categoryModel = storeDB.Categories.Include("Items")
+                .Single(c=>c.Name==category);
+            return View(categoryModel);
+        }
+        public ActionResult Details(int id)
+        {
+            var Item = storeDB.Items.Find(id);
+            return View(Item);
+        }
+    }
+}
